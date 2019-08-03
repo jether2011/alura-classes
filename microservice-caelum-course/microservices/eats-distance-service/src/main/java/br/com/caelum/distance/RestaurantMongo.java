f13c1149a68ec58e4ed9932de0ef7ed52f67edb5
Jether Rodrigues <jether.rodrigues@geovoxel.com>
2019-08-03 10:43:54 -0300

    refactoring the service to use mongodb

--- microservice-caelum-course/microservices/eats-distance-service/src/main/java/br/com/caelum/distance/RestaurantMongo.java
+++ microservice-caelum-course/microservices/eats-distance-service/src/main/java/br/com/caelum/distance/RestaurantMongo.java
@@ -5,24 +5,23 @@ import javax.validation.constraints.Size;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Document(collection = "restaurants")
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-public class Restaurant {
+public class RestaurantMongo {
 
 	@Id
 	private Long id;
 
 	@NotBlank @Size(max=9)
 	private String cep;
-
-	@Indexed
-	private Boolean approved;
 	
 	@Indexed
 	private Long cookId;
