de2ceb0acc83393cbd92d5adfecf11e5e00d78ed
Jether Rodrigues <jetherrodrigues@gmail.com>
2019-08-17 16:14:43 -0300

    updated to consume the queue and, if fail, fail siliently

--- microservice-caelum-course/microservices/fj33-eats-nota-fiscal-service/src/main/java/br/com/caelum/notafiscal/services/ProcessadorDePagamentos.java
+++ microservice-caelum-course/microservices/fj33-eats-nota-fiscal-service/src/main/java/br/com/caelum/notafiscal/services/ProcessadorDePagamentos.java
@@ -1,11 +1,15 @@
 package br.com.caelum.notafiscal.services;
 
+import org.springframework.cloud.stream.annotation.StreamListener;
 import org.springframework.stereotype.Service;
 
+import br.com.caelum.notafiscal.AMQPConfig.PaymentConsumer;
 import br.com.caelum.notafiscal.pedido.PedidoDto;
 import br.com.caelum.notafiscal.pedido.PedidoRestClient;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class ProcessadorDePagamentos {
@@ -13,9 +17,19 @@ public class ProcessadorDePagamentos {
 	private GeradorDeNotaFiscal notaFiscal;
 	private PedidoRestClient pedidos;
 
-	public void processaPagamento(PagamentoConfirmado pagamento) {
-		PedidoDto pedido = pedidos.detalhaPorId(pagamento.getPedidoId());
-		String nota = notaFiscal.geraNotaPara(pedido);
-		System.out.println(nota); // TODO: enviar XML para SEFAZ
+	@StreamListener(PaymentConsumer.PAYMENTS_CONFIRMED)
+	public void processaPagamento(PaymentConfirmedDTO payment) {
+		log.info("Received: {}", payment);
+		tryRetrieveOrderAndGenerateNF(payment); 
+	}
+
+	private void tryRetrieveOrderAndGenerateNF(PaymentConfirmedDTO payment) {
+		try {
+			PedidoDto pedido = pedidos.detalhaPorId(payment.getOrderId());
+			String nota = notaFiscal.geraNotaPara(pedido);
+			System.out.println(nota);
+		} catch (Exception e) {
+			log.error("Error to consuming monolith! [{}]", e.getMessage());
+		}		
 	}
 }
