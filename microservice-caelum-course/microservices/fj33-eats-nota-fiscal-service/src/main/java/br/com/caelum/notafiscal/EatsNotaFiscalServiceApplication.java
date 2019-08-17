f35aa97e6a59d40315d5ca6fff04ae5297c32372
Jether Rodrigues <jetherrodrigues@gmail.com>
2019-08-17 16:12:15 -0300

    enabled auto configuration

--- microservice-caelum-course/microservices/fj33-eats-nota-fiscal-service/src/main/java/br/com/caelum/notafiscal/EatsNotaFiscalServiceApplication.java
+++ microservice-caelum-course/microservices/fj33-eats-nota-fiscal-service/src/main/java/br/com/caelum/notafiscal/EatsNotaFiscalServiceApplication.java
@@ -1,6 +1,7 @@
 package br.com.caelum.notafiscal;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableAutoConfiguration
 public class EatsNotaFiscalServiceApplication {
 
 	public static void main(String[] args) {
