f27aea87f71502f9c0ba2c2208970e6b1b8e7358
Jether Rodrigues <jetherrodrigues@gmail.com>
2019-08-17 15:59:16 -0300

    enabled auto configuration

--- microservice-caelum-course/microservices/eats-payment-service/src/main/java/br/com/caelum/EatsPaymentServiceApplication.java
+++ microservice-caelum-course/microservices/eats-payment-service/src/main/java/br/com/caelum/EatsPaymentServiceApplication.java
@@ -1,6 +1,7 @@
 package br.com.caelum;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -8,6 +9,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableAutoConfiguration
 public class EatsPaymentServiceApplication {
 
 	public static void main(String[] args) {
